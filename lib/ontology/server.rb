36057998d556b5d2d82fe5559f38bb4f8bc644a1
jweissman <joseph.weissman@tapjoy.com>
2013-04-03 03:30:44 -0400

    Update: 2-way rpc :)

--- lib/ontology/server.rb
+++ lib/ontology/server.rb
@@ -22,25 +22,24 @@ class Server < Goliath::WebSocket
   def on_message(env, msg)
     env.logger.info "WS MESSAGE: #{msg}"
 
+    #env['goliath.request-headers'].each_pair do |key, value|
+    #  puts ">>> #{key} => #{value}"
+    #end
     body = JSON[msg]
     command = body['command']
 
-    return {:error => 201, :message => 'No command given'} unless command
-
-    result = if command == 'chat'
-      nickname, message = body['nickname'], body['message']
-      if nickname && message
-        channel << "(#{nickname}) #{message}"
-        {:ok => 100}
-      else
-        {:error => 210, :message => "Bad chat command", :detail => "Must provide nickname and message"}
-      end
-    elsif command == 'world'
-      {:ok => 100, :value => World.current.state.value}
-    else
-      {:error => 202, :message => "Unknown command '#{command}'"}
+    return unless command
+                        
+    #player_name = body['name']
+    if command == 'chat'
+      name, message = body['name'], body['message']
+      #channel << "(#{name}) #{message}" if name && message
+      channel << {:command => 'chat', :name => name, :message => message}.to_json if name && message
+    elsif command == 'join'
+      name = body['name']
+      World.current.async.add_player(name)
+      channel << { :command => 'players', :players => World.current.players.map(&:name) }.to_json
     end
-    result
   end
 
   def on_close(env)
@@ -66,7 +65,7 @@ $stdout.sync = true
 # kick off world simulation... (probably best in a separate process, but for now anyway...)
 World.current.every(0.5) do
   World.current.simulate #(env)
-  if World.current.state.value % 10 == 0
-    CHANNEL << '...'
-  end
+#  if World.current.state.value % 10 == 0
+#    CHANNEL << '...'
+#  end
 end
--- lib/ontology/world.rb
+++ lib/ontology/world.rb
@@ -1,16 +1,74 @@
+class Player
+  attr_accessor :name, :position
+  def initialize(name, position)
+    @name = name
+    @position = position
+  end
+end
+
+class Map
+  attr_accessor :cells, :height, :width
+  def initialize(opts={})
+    @height = opts[:height] || 10
+    @width  = opts[:width]  || 10
+    @cells  = Array.new(@height) { Array.new(@width) {0}}
+  end
+
+  def each_position
+    @width.times do |x|
+      @height.times do |y|
+        yield [x,y]
+      end
+    end
+  end
+
+  def each_open_position(player_positions)
+    each_position do |position|
+      yield position unless player_positions.include?(position)
+    end
+  end
+end
+
+
 class World
   include Celluloid
-
-  attr_accessor :state
+  attr_accessor :players, :map
   def initialize
-    @state = OpenStruct.new
-    @state.value = 0
-    @state.players = []
-    @state.map = []
+    @players = []
+    @map = Map.new
+  end
+
+  def open_positions
+    open = []
+    @map.each_open_position(@players.map(&:position)) { |p| open << p }
+    open
+  end
+
+  def add_player name
+    @players << Player.new(name, open_positions.sample)
+  end
+
+  def player_named name
+    @players.reject { |p| p.name != name }.first
+  end
+
+  COMPASS = {n: [0,-1], e: [1,0], s: [0,1], w:[-1,0] }
+  def translate_position position, direction
+    position + COMPASS[direction.slice(0,1).downcase.to_sym]
+  end
+
+  def move_player name, direction
+    target_cell = translate_position(player_named(name).position, direction)
+    raise "Invalid move" unless open_positions.include? target_cell
+    player_named(name).position = target_cell
   end
 
   def simulate
-    @state.value += 1
+    #@state.value += 1
+
+    # currently a no-op :(
+
+    # it's okay! :) promise...
   end
 
   class << self
